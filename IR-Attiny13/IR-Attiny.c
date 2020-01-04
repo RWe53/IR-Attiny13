9db1f26038af5de1eff6a1785e2353da28d37421
RWe53 <59062852+RWe53@users.noreply.github.com>
2020-01-04 19:57:06 +0100

    Update IR-Attiny.c

--- IR-Attiny13/IR-Attiny.c
+++ IR-Attiny13/IR-Attiny.c
@@ -14,15 +14,15 @@ extern uint8_t IR_RAM[];
 
 int main(void)
 {
-	OSCCAL =0x7F;										// CPU 2*9,6mHz
+	OSCCAL =0x7F;		// CPU 2*9,6mHz
 	CLKPR=_BV(CLKPCE);
 	CLKPR=0;	
-	DDRB		= _BV(PB4) ;
-	PORTB		= _BV(PB1) ;
-	TCCR0B	= _BV(CS02) | _BV(CS00);
+	DDRB	= _BV(PB4) ;
+	PORTB	=  _BV(PB1) ;
+	TCCR0B	=  _BV(CS02) | _BV(CS00);
 	TCNT0=0;
 	OCR0B=2;
-	TIMSK0	= _BV(OCIE0B);
+	TIMSK0 = _BV(OCIE0B);
 	GIMSK = _BV(INT0);
 	MCUCR = _BV(ISC01);
 	sei();
@@ -32,7 +32,7 @@ int main(void)
 	
     while (1) 
     {
-			 if (IR_RAM[0] & (1<<1) && IR_RAM[4]==0x40)	PORTB		|= _BV(PB4); else PORTB &=~(1<<PB4);
+	if (IR_RAM[0] & (1<<1) && IR_RAM[4]==0x40)	PORTB		|= _BV(PB4); else PORTB &=~(1<<PB4);
     
     }
 }
